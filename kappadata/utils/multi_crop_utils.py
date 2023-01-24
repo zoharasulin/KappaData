740ebcd2122d8dce7c2520ed0885919aa1d2872f
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-01-24 01:38:34 +0100

    multicrop

--- kappadata/utils/multi_crop_utils.py
+++ kappadata/utils/multi_crop_utils.py
@@ -1,6 +1,6 @@
 import torch
 from collections import defaultdict
-
+from itertools import chain
 
 class MultiCropJointForwardModule(torch.nn.Module):
     def __init__(self, module):
@@ -38,20 +38,47 @@ def multi_crop_joint_forward(model, *args, **kwargs):
 def concat_same_shapes(*args, **kwargs):
     # collect same shapes
     concat_args = []
+    concat_args_idxs = defaultdict(lambda: defaultdict(list))
     for i in range(len(args)):
         if isinstance(args[i], (list, tuple)) and torch.is_tensor(args[i][0]):
             items_by_shape = defaultdict(list)
-            for item in args[i]:
+            for j, item in enumerate(args[i]):
                 items_by_shape[item.shape].append(item)
+                concat_args_idxs[i][item.shape].append(j)
             
             if len(items_by_shape) == 1:
                 concat_args.append(torch.concat(list(items_by_shape.values())[0]))
             else:
+                concat_args.append([torch.concat(values) for values in items_by_shape.values()])
 
         else:
             concat_args.append(args[i])
 
+    # flatten indices
+    concat_args_idxs_flat = {}
+    for k, v in concat_args_idxs.items():
+        concat_args_idxs_flat[k] = [vv for vv in v.values()]
 
+    return concat_args, concat_args_idxs_flat, kwargs
+
+def split_same_shapes(concat_args, concat_args_idxs, kwargs):
+    # reverse the concat_same_shapes operation
+    split_args = []
+    for arg_idx in range(len(concat_args)):
+        idxs = concat_args_idxs[arg_idx]
+        results = []
+        # chunk concated tensor
+        for group_idx in range(len(concat_args[arg_idx])):
+            results += concat_args[arg_idx][group_idx].chunk(len(idxs[group_idx]))
+        # unshuffle
+        flat_idxs = list(chain(*idxs))
+        if len(flat_idxs) == 1:
+            split_args.append(results[0])
+        else:
+            split_args.append([results[i] for i in flat_idxs])
+
+    # TODO kwargs
+    return split_args, kwargs
 
 
 def multi_crop_split_forward(model, x, n_chunks=None):
--- tests_unit/utils/test_multi_crop_utils.py
+++ tests_unit/utils/test_multi_crop_utils.py
@@ -6,6 +6,8 @@ from kappadata.utils.multi_crop_utils import (
     multi_crop_joint_forward,
     MultiCropSplitForwardModule,
     MultiCropJointForwardModule,
+    concat_same_shapes,
+    split_same_shapes,
 )
 from tests_util.modules.memorize_shape_module import MemorizeShapeModule
 
@@ -45,3 +47,15 @@ class TestMultiCropUtils(unittest.TestCase):
         self.assertEqual((10, 4), model[0].module.shapes[0])
         self.assertEqual((5, 8), model[1].module.shapes[0])
         self.assertEqual((5, 8), model[1].module.shapes[1])
+
+    def test_concat_same_shape_unshuffle(self):
+        rng = torch.Generator().manual_seed(5)
+        x0 = torch.randn(3, 2, generator=rng)
+        x1 = torch.randn(3, 3, generator=rng)
+        x2 = torch.randn(3, 2, generator=rng)
+        args, args_idxs, kwargs = concat_same_shapes([x0, x1, x2])
+        result_args, result_kwargs = split_same_shapes(args, args_idxs, kwargs)
+        self.assertEquals(x0.tolist(), result_args[0][0].tolist())
+        self.assertEquals(x1.tolist(), result_args[0][1].tolist())
+        self.assertEquals(x2.tolist(), result_args[0][2].tolist())
+        # TODO kwargs
